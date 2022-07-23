23d53c210b13b5d1d4c16204880a91f3543932a2
Nesterenko-Artem <petr79petrov1979@gmail.com>
2022-07-23 22:31:29 +0300

    fix task 05

--- js/task-05.js
+++ js/task-05.js
@@ -4,3 +4,15 @@
 // <input type="text" id="name-input" placeholder="Please enter your name" />
 // <h1>Hello, <span id="name-output">Anonymous</span>!</h1>
 // ===========================
+const ref = {
+  input: document.querySelector("#name-input"),
+  span: document.querySelector("#name-output"),
+};
+const textSpanDef = "Anonymous";
+
+function valueInput(event) {
+  event.currentTarget.value === ""
+    ? (ref.span.textContent = textSpanDef)
+    : (ref.span.textContent = event.currentTarget.value);
+}
+ref.input.addEventListener("input", valueInput);
